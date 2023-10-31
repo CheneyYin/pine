d9cc8e7cc400b52774edc6570e87ac3e0c846784
Chengyu Yan <cheneyyin@hotmail.com>
2023-10-31 03:22:58 +0000

    fix(jest): fix jest module no found

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+});
+
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    roots: ['<rootDir>'],
+    modulePaths: [compilerOptions.baseUrl],
+    moduleNameMapper: moduleNameMapper,
 };
+console.info(moduleNameMapper);
