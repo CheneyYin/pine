ce82b35d26eff0067034099811e60fcb3223319f
Chengyu Yan <cheneyyin@hotmail.com>
2023-10-31 06:29:56 +0000

    feat(logger): add logger module

---
+++ packages/logger/src/index.ts
@@ -0,0 +1,15 @@
+import {
+    LoggerOptions,
+    createLogger,
+    format,
+    transports,
+    Logger,
+} from 'winston';
+
+const opts: LoggerOptions = {
+    level: 'info',
+    format: format.simple(),
+    transports: [new transports.Console()],
+};
+
+export const logger: Logger = createLogger(opts);
