3e25e4ff82b0fcbfceb6b21836bdfc18a3ae38e1
Chengyu Yan <cheneyyin@hotmail.com>
2023-10-31 05:58:11 +0000

    refactor(eslint): remove redundant config about eslint

--- packages/common/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-module.exports = {
-    extends: ['../../.eslintrc.cjs'],
-    parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ['./tsconfig.json'],
-    },
-};
